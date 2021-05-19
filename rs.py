d79f51f5b387aa8db8caaa46d4cd84140c20e73c
tomek <tknaur@gmail.com>
2021-05-19 19:32:27 +0200

    Small changes in UI. :-)

--- rs.py
+++ rs.py
@@ -25,7 +25,7 @@ config = {
 
 
 class Response(Enum):
-    OK = dict(code=200, msg='-> Hello world!\n----------------------------\nAvailable URIs: \n{0}'.format(
+    OK = dict(code=200, msg='🞺 Hello world 🞺\n\nAvailable URIs: \n{0}'.format(
         ' \n'.join(sorted(config.keys(), reverse=False))))
     FAIL = dict(code=404, msg='Something is wrong here.')
 
@@ -51,7 +51,7 @@ class RadioStationHandler(BaseHTTPRequestHandler):
         self.send_header("Content-type", "text/plain; charset=utf-8")
 
         self.end_headers()
-        o = response.value['msg'] + '\n----------------------------\n' + output
+        o = response.value['msg'] + '\n\n' + output
         self.wfile.write(o.encode())
         return
 
